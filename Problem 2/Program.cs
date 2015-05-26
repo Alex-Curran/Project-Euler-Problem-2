a2e432485e4866ee970f032a3b464c14a94c1725
Alex-Curran <alexcurran839@gmail.com>
2015-05-25 20:38:35 -0500

    First Implementation

--- Problem 2/Program.cs
+++ Problem 2/Program.cs
@@ -21,6 +21,46 @@ namespace Problem_2
     {
         static void Main(string[] args)
         {
+            long upperLimit = 4000000;
+            long sum = sumEvenFibonacci(upperLimit);
+            Console.WriteLine("The sum of even fibonacci numbers under {0:n0} = {1:n0}", upperLimit, sum);
         }
+
+
+        /// <summary>
+        ///  Computes fibonacci numbers. Then adds the even numbers. 
+        ///  This is the first implementation. There are better and faster ways to do this!
+        /// </summary>
+        /// <param name="limit"> Upper limit. Used as stopping point for calculation</param>
+        /// <returns> Returns the sum of the even fibonacci numbers</returns>
+        private static long sumEvenFibonacci(long limit)
+        {
+            long sum = 0; 
+
+            List<long> Fibonacci = new List<long>();
+            Fibonacci.Add(1);
+            Fibonacci.Add(2);
+
+            // Adds Fibonacci numbers < limit to a List 
+            int i = 1;
+            while (Fibonacci[i] + Fibonacci[i - 1] < limit)
+            {
+                Fibonacci.Add(Fibonacci[i] + Fibonacci[i - 1]);
+                i++;
+            }
+
+            //Get the even numbers 
+            var evenFibonacci =     (from fib in Fibonacci
+                                     where fib % 2 == 0
+                                     select fib).ToList();
+            foreach (long value in evenFibonacci)
+            {
+                sum += value;
+            }
+
+            return sum;
+
+        }
+
     }
 }
