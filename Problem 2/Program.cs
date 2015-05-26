033bc5adedb0630d10747601f585e58020004cdc
Alex-Curran <alexcurran839@gmail.com>
2015-05-25 19:50:32 -0500

    Project Set Up

---
+++ Problem 2/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ *   Project Euler: Problem 2
+ *   Alex Curran 
+ *   
+ *   By considering the terms in the Fibonacci sequence whose values do not exceed four million,
+ *   find the sum of the even-valued terms.
+ *   
+ *  https://projecteuler.net/problem=2
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
